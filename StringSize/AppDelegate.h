b0c8fec52d9f2a9ca8ecc2aacd1a99ac62731f44
yxw-duopai <yxw851027@163.com>
2015-08-26 14:14:59 +0800

    Initial Commit

---
+++ StringSize/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StringSize
+//
+//  Created by yxw on 15/8/26.
+//  Copyright (c) 2015年 yxw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StringSize/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  StringSize
+//
+//  Created by yxw on 15/8/26.
+//  Copyright (c) 2015年 yxw. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
